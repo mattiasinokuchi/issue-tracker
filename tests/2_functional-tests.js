ee398236684198d125cc51508755aa130b0f500a
Mattias Inokuchi <8-b@tuta.io>
2020-11-26 13:22:21 +0000

    completed test 'one filter'

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -102,19 +102,11 @@ suite('Functional Tests', function() {
     test('One filter', function(done) {
       chai.request(server)
       .get('/api/issues/test')
-      .query({})
+      .query({open: true})
       .end(function(err, res){
         assert.equal(res.status, 200);
         assert.isArray(res.body);
-        assert.property(res.body[0], 'issue_title');
-        assert.property(res.body[0], 'issue_text');
-        assert.property(res.body[0], 'created_on');
-        assert.property(res.body[0], 'updated_on');
-        assert.property(res.body[0], 'created_by');
-        assert.property(res.body[0], 'assigned_to');
-        assert.property(res.body[0], 'open');
-        assert.property(res.body[0], 'status_text');
-        assert.property(res.body[0], '_id');
+        assert.equal(res.body[0].open.toString(), 'true');
         done();
       });
     });
